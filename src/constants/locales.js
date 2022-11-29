a24c7233e99b472fa48584014e77832b918b5493
ondrui <ondrui@yandex.ru>
2022-11-29 17:57:11 +0300

    add constants locales

--- src/constants/locales.js
+++ src/constants/locales.js
@@ -1,5 +1,59 @@
 export const expressions = {
-  ru: {},
-  en: {},
+  ru: {
+    breadcrumb: {
+      main: "главная",
+      weather: "погода",
+    },
+    header: {
+      now: "сейчас в",
+      forecast: "по прогнозу",
+      realFeel: "ощущается",
+    },
+    tabsDescr: {
+      hourly: "",
+      daily: "",
+    },
+    climateIndicators: {
+      temperature: "",
+      pressure: "",
+      wind: "",
+      windDirSpeed: "",
+      gusts: "",
+      humidity: "",
+      precSum: "",
+      precProb: "",
+      uvi: "",
+      daytime: "",
+    },
+  },
+  en: {
+    en: {
+      breadcrumb: {
+        main: "главная",
+        weather: "погода",
+      },
+      header: {
+        now: "сейчас в",
+        forecast: "по прогнозу",
+        realFeel: "ощущается",
+      },
+      tabsDescr: {
+        hourly: "",
+        daily: "",
+      },
+      climateIndicators: {
+        temperature: "",
+        pressure: "",
+        wind: "",
+        windDirSpeed: "",
+        gusts: "",
+        humidity: "",
+        precSum: "",
+        precProb: "",
+        uvi: "",
+        daytime: "",
+      },
+    },
+  },
   fr: {},
 };
