7980bdec0f1589be8d2169dff0a7564d2357ddf7
ondrui <ondrui@yandex.ru>
2022-12-16 00:17:09 +0300

    refactoring svg render function in SVGChartsItem.vue

--- src/components/SVGChartsItem.vue
+++ src/components/SVGChartsItem.vue
@@ -1,6 +1,6 @@
 <template>
   <g>
-    <path stroke="#0BC2FF" stroke-width="2" :d="makePath"></path>
+    <path stroke="#0BC2FF" stroke-width="2" :d="svgPath"></path>
     <circle
       :cx="p.x"
       :cy="p.y"
@@ -53,17 +53,15 @@ export default {
       console.log(points);
       return points;
     },
-    makePath() {
-      const points = this.calcPointsSVG;
-      let catmull = this.catmullRom2bezier(points);
-      console.log(catmull);
-      const d = catmull.reduce(
-        (acc, point, i) =>
+    svgPath() {
+      // build the d attributes by looping over the points
+      const d = this.calcPointsSVG.reduce(
+        (acc, point, i, a) =>
           i === 0
             ? // if first point
-              `M ${points[0]},${points[1]}`
+              `M ${point.x},${point.y}`
             : // else
-              `${acc} C ${point[0].x},${point[0].y} ${point[1].x},${point[1].y} ${point[2].x},${point[2].y}`,
+              `${acc} ${this.catmullRom2bezier(a, i - 1)}`,
         ""
       );
       return `${d}`;
@@ -83,51 +81,47 @@ export default {
       );
       return y;
     },
-    catmullRom2bezier(points) {
-      let result = [];
-      for (let i = 0; i < points.length - 1; i++) {
-        let p = [];
+    catmullRom2bezier(points, i) {
+      let p = [];
 
-        p.push({
-          x: points[Math.max(i - 1, 0)].x,
-          y: points[Math.max(i - 1, 0)].y,
-        });
-        p.push({
-          x: points[i].x,
-          y: points[i].y,
-        });
-        p.push({
-          x: points[i + 1].x,
-          y: points[i + 1].y,
-        });
-        p.push({
-          x: points[Math.min(i + 2, points.length - 1)].x,
-          y: points[Math.min(i + 2, points.length - 1)].y,
-        });
+      p.push({
+        x: points[Math.max(i - 1, 0)].x,
+        y: points[Math.max(i - 1, 0)].y,
+      });
+      p.push({
+        x: points[i].x,
+        y: points[i].y,
+      });
+      p.push({
+        x: points[i + 1].x,
+        y: points[i + 1].y,
+      });
+      p.push({
+        x: points[Math.min(i + 2, points.length - 1)].x,
+        y: points[Math.min(i + 2, points.length - 1)].y,
+      });
 
-        // Catmull-Rom to Cubic Bezier conversion matrix
-        //    0       1       0       0
-        //  -1/6      1      1/6      0
-        //    0      1/6      1     -1/6
-        //    0       0       1       0
+      // Catmull-Rom to Cubic Bezier conversion matrix
+      //    0       1       0       0
+      //  -1/6      1      1/6      0
+      //    0      1/6      1     -1/6
+      //    0       0       1       0
 
-        let bp = [];
-        bp.push({
-          x: (-p[0].x + 6 * p[1].x + p[2].x) / 6,
-          y: (-p[0].y + 6 * p[1].y + p[2].y) / 6,
-        });
-        bp.push({
-          x: (p[1].x + 6 * p[2].x - p[3].x) / 6,
-          y: (p[1].y + 6 * p[2].y - p[3].y) / 6,
-        });
-        bp.push({
-          x: p[2].x,
-          y: p[2].y,
-        });
-        result.push(bp);
-      }
-
-      return result;
+      let bp = [];
+      bp.push({
+        x: (-p[0].x + 6 * p[1].x + p[2].x) / 6,
+        y: (-p[0].y + 6 * p[1].y + p[2].y) / 6,
+      });
+      bp.push({
+        x: (p[1].x + 6 * p[2].x - p[3].x) / 6,
+        y: (p[1].y + 6 * p[2].y - p[3].y) / 6,
+      });
+      bp.push({
+        x: p[2].x,
+        y: p[2].y,
+      });
+      console.log(bp);
+      return `C ${bp[0].x},${bp[0].y} ${bp[1].x},${bp[1].y} ${bp[2].x},${bp[2].y}`;
     },
   },
 };
